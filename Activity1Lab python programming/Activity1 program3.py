5e8e4e303705a6a3c2a4b145e649a522e3adeca5
mohammadsahib013 <75753063+mohammadsahib013@users.noreply.github.com>
2022-09-19 11:13:39 +0530

    Add files via upload

---
+++ Activity1Lab python programming/Activity1 program3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Finding largest number in a list using sort method
+
+list1 = [4, 1, 7, 3, 2, 5]
+
+list1 = sorted(list1, reverse=True)
+print("Largest number in list is", list1[0])
+
+
+# In[ ]:
+
+
+
+
